fd89c20482e27c4e190a67c4a647d4a50f72e4be
mjzzanichelli <mjzzanichelli@hotmail.com>
2021-02-04 13:25:46 +0000

    removed babel, upgraded deps

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-/**
- * @file Firepocess initiator through command lin
- * @author José Marco Zanichelli
- * @version: 0.1
- */
-var argv = require('minimist')(process.argv.slice(2)),
-  processor = require("./src/processor")
-  ;
+const colors = require('colors/safe');
 
-processor(argv);
+let log = function (...args) {
+  return console.log(colors.bgCyan.black(...args));
+};
+
+let highlight = function (...args) {
+  return console.log(colors.bgMagenta.white(...args));
+};
+
+let error = function (...args) {
+  return console.log(colors.bgRed.white(...args));
+};
+
+module.exports = {
+  log,
+  highlight,
+  error,
+};
